32f3b98dcea59713543239bb2888c9b4477d7db1
Maurus Cuelenaere <mcuelenaere@gmail.com>
2018-10-02 12:24:08 +0200

    Remove hardcoded number of maxconnections + move dependencies install from init script to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM debian:latest
+
+# install deps
+RUN apt update && apt upgrade -y && apt install -y curl gpg ca-certificates tar dirmngr
+
 ADD init.sh /
+
 EXPOSE 22556
 ENTRYPOINT ["/bin/bash"]
 CMD ["/init.sh"]
--- init.sh
+++ init.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-## Update packages
-apt update && apt upgrade -y && apt install -y curl gpg ca-certificates tar dirmngr
 ## Check if binaries are already installed
 cd ~
 if [ ! -f ~/dogecoin-bin/bin/dogecoind ]; then
     curl -o dogecoin.tar.gz -Lk https://github.com/dogecoin/dogecoin/releases/download/v1.10.0/dogecoin-1.10.0-linux64.tar.gz
     if [ $? -eq 0 ]; then
         tar -xvf dogecoin.tar.gz
-        ##Rename folder appropriately
+        ## Rename folder appropriately
         mv dogecoin-1.10.0 dogecoin-bin
-        ##Add dogecoind commands to PATH
+        ## Add dogecoind commands to PATH
         echo 'export PATH=$PATH:~/dogecoin-bin/bin/' > ~/.bashrc
         source ~/.bashrc
         rm dogecoin.tar.gz
     else
         rm dogecoin.tar.gz
-        echo "Download failed. Restarting script in 5 seconds"
-        sleep 5
-        exec /init.sh
+        echo "Download failed."
+        exit 1
     fi
 fi
 
@@ -29,6 +26,7 @@ if [ ! -f ~/.dogecoin/dogecoin.conf ]; then
     PWord=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1`
     echo rpcpassword=$PWord >> ~/.dogecoin/dogecoin.conf
 fi
+
 ##Remove bootstrap.dat.old if it exists
 if [ -f ~/.dogecoin/bootstrap.dat.old ]; then
     rm ~/.dogecoin/bootstrap.dat.old
@@ -36,4 +34,4 @@ fi
 
 ##Start dogecoind daemon
 echo Running dogecoin
-~/dogecoin-bin/bin/dogecoind -maxconnections=500 -printtoconsole -shrinkdebugfile
+~/dogecoin-bin/bin/dogecoind -printtoconsole -shrinkdebugfile
